396631d899bf20beff8906afecd6381d6d34d40d
xianfanjie <102248059+xianfanjie@users.noreply.github.com>
2024-03-29 21:45:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,8 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
+rm -rf package/feeds/luci/luci-app-ssr-plus
+rm -rf package/feeds/luci/luci-app-filetransfer
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
