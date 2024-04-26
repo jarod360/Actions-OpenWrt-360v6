d264d0975747150654c04adf9d94a8c42d397390
jarod360 <32123167+jarod360@users.noreply.github.com>
2024-04-26 17:16:24 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -15,6 +15,7 @@
 rm -rf feeds/packages/libs/libwebsockets
 rm -rf feeds/luci/applications/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
+rm -rf feeds/luci/applications/luci-app-serverchan
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
