2858135c3e6217f73635d56b017006512f3f2ee2
jarod360 <32123167+jarod360@users.noreply.github.com>
2024-05-08 18:46:25 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -40,4 +40,4 @@ sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=360V6-$(shell TZ=UTC
 git clone https://github.com/jarod360/luci-app-ttyd package/luci-app-ttyd
 git clone https://github.com/jarod360/packages package/mypackge
 git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git packge/luci-app-serverchan
-git clone https://github.com/jarod360/luci-app-xupnpd
+git clone https://github.com/jarod360/luci-app-xupnpd package/luci-app-xupnpd
