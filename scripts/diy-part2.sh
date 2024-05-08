bbc16f22c7f32effdfdf8d56f270c2199bd4b5f6
jarod360 <32123167+jarod360@users.noreply.github.com>
2024-05-08 23:09:25 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -17,6 +17,8 @@ rm -rf feeds/luci/applications/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-msd_lite
+rm -rf feeds/packages/net/msd_lite
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
@@ -42,7 +44,9 @@ git clone https://github.com/jarod360/luci-app-ttyd package/luci-app-ttyd
 git clone https://github.com/jarod360/packages package/mypackge
 git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git packge/luci-app-serverchan
 git clone https://github.com/jarod360/luci-app-xupnpd package/luci-app-xupnpd
+git clone https://github.com/jarod360/luci-app-msd_lite package/luci-app-msd_lite
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+svn co https://github.com/coolsnowwolf/packages/trunk/net/msd_lite package/msd_lite
 
 # 调整argon登录框为居中
 sed -i "/.login-page {/i\\
