cc6ba2c0d42193a5e4cd67fc637380f66cc25e9e
jarod360 <32123167+jarod360@users.noreply.github.com>
2024-05-09 01:10:40 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -17,8 +17,8 @@ rm -rf feeds/luci/applications/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-msd_lite
-rm -rf feeds/packages/net/msd_lite
+#rm -rf feeds/luci/applications/luci-app-msd_lite
+#rm -rf feeds/packages/net/msd_lite
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.254/g' package/base-files/files/bin/config_generate
@@ -44,18 +44,18 @@ git clone https://github.com/jarod360/luci-app-ttyd package/luci-app-ttyd
 git clone https://github.com/jarod360/packages package/mypackge
 git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git packge/luci-app-serverchan
 git clone https://github.com/jarod360/luci-app-xupnpd package/luci-app-xupnpd
-git clone https://github.com/jarod360/luci-app-msd_lite package/luci-app-msd_lite
+#git clone https://github.com/jarod360/luci-app-msd_lite package/luci-app-msd_lite
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-curl -L -o msd_lite.tar.gz https://github.com/coolsnowwolf/packages/archive/master.tar.gz
-tar -xzf msd_lite.tar.gz
-mv packages-master/net/msd_lite/ package/msd_lite
-rm msd_lite.tar.gz
+#curl -L -o msd_lite.tar.gz https://github.com/coolsnowwolf/packages/archive/master.tar.gz
+#tar -xzf msd_lite.tar.gz
+#mv packages-master/net/msd_lite/ package/msd_lite
+#rm msd_lite.tar.gz
 
 # 调整argon登录框为居中
 sed -i "/.login-page {/i\\
 .login-container {\n\
   margin: auto;\n\
-  height: 420px\!important;\n\
+  height: 520px\!important;\n\
   min-height: 420px\!important;\n\
   left: 0;\n\
   right: 0;\n\
