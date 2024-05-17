6aec03041b57eb52367932af276a50552cdc5b2d
jarod360 <32123167+jarod360@users.noreply.github.com>
2024-05-17 12:33:57 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -24,10 +24,10 @@ rm -rf feeds/kenzo/luci-app-wechatpush
 rm -rf feeds/small/chinadns-ng
 rm -rf feeds/kenzo/luci-app-alist
 
-# 修改版本信息
-date=`date +%y.%m.%d`
-sed -i 's/OpenWrt/OpenWrt By Jarod /g' package/addition/default-settings/files/99-default-settings
-sed -i 's/R23.11.20/R'$date'/g' package/addition/default-settings/files/99-default-settings
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by Jarod/g" package/addition/default-settings/files/99-default-settings
 
 # 修改本地时间格式
 sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/addition/autocore/files/*/index.htm
